736be0f3b0c4b5052d066fcd94dc2538bee1ac35
Sanjay Mali <malisanjay5433@users.noreply.github.com>
2021-05-28 12:21:16 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .target(
             name: "SwiftPageMenu",
-            path: "./Source"
+            path: "./SwiftPageMenu/Source"
         )
     ]
 )
