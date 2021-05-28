a6b83e8011c9c3b2e35623c88a0f88f4fc7da1b9
Sanjay Mali <malisanjay5433@users.noreply.github.com>
2021-05-28 12:18:16 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,1 +1,16 @@
 
+import PackageDescription
+
+let package = Package(
+    name: "SwiftPageMenu",
+    products: [
+        .library(name: "SwiftPageMenu", targets: ["SwiftPageMenu"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftPageMenu",
+            path: "./SwiftPageMenu/Source"
+        )
+    ]
+)
